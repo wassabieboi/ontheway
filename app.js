76aec16eced74d3593f8dfa2801f582e983d583f
Derek Lau <dlau@linkedin.com>
2015-01-30 14:45:16 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+/*
+ * Module dependencies
+ */
+var express = require('express'),
+    morgan = require('morgan'),
+    stylus = require('stylus'),
+    nib = require('nib')
+
+var app = express()
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(morgan('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index',
+    { title: 'OnTheWay Home' })
+})
+app.listen(3000)
